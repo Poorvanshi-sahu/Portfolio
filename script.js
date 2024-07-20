33e0db4f2083eb405fe4f0d53c7fbd48a2f6b20f
Poorvanshi <poorvanshisahu837@gmail.com>
2024-07-20 21:26:12 +0530

    more changes to menu height

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const cursor = new MouseFollower({
   speed: 0.6,
 });
 
-document.addEventListener('contextmenu', (e)=>{
-  e.preventDefault();
-})
+// document.addEventListener('contextmenu', (e)=>{
+//   e.preventDefault();
+// })
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "C")) {
-    e.preventDefault();
-  }
-});
+// document.addEventListener("keydown", (e) => {
+//   if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "C")) {
+//     e.preventDefault();
+//   }
+// });
 
 gsap.registerPlugin("ScrollTrigger");
 const scroller = new LocomotiveScroll({
